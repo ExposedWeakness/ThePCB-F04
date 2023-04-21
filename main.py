53c948259b338b01fae84ede403248ed2d6a7b29
ExposedWeakness <81777256+ExposedWeakness@users.noreply.github.com>
2023-04-21 14:58:07 -0400

    Refactor .py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 def wrong():
     global input2
     input2 = 0
-    for index in range(5):
+    for index in range(3):
         basic.show_icon(IconNames.NO)
         music.play_sound_effect(music.create_sound_effect(WaveShape.SQUARE,
-                1,
+                63,
                 5000,
                 255,
-                255,
+                0,
                 500,
                 SoundExpressionEffect.NONE,
                 InterpolationCurve.LOGARITHMIC),
@@ -20,38 +20,69 @@ def wrong():
                         . . . . .
         """)
 
-def on_button_pressed_a():
+def on_pin_pressed_p0():
     global input2
+    basic.show_leds("""
+        . . # . .
+                . # # . .
+                . . # . .
+                . . # . .
+                . # # # .
+    """)
     if input2 == 0:
         input2 = 1
     else:
-        if input2 == 12:
-            input2 = 123
-        else:
-            wrong()
-input.on_button_pressed(Button.A, on_button_pressed_a)
+        wrong()
+input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
 
-def on_button_pressed_b():
+def on_pin_pressed_p2():
     global input2
+    basic.show_leds("""
+        . # # # .
+                . . . # .
+                . # # # .
+                . . . # .
+                . # # # .
+    """)
     if input2 == 1:
-        input2 = 12
+        input2 = 13
     else:
-        if input2 == 123:
-            input2 = 1234
-        else:
-            wrong()
-input.on_button_pressed(Button.B, on_button_pressed_b)
+        wrong()
+input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
+
+def on_pin_pressed_p1():
+    global input2
+    basic.show_leds("""
+        . # # # .
+                . . . # .
+                . # # # .
+                . # . . .
+                . # # # .
+    """)
+    if input2 == 13:
+        input2 = 132
+    else:
+        wrong()
+input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
 
 input2 = 0
 input2 = 0
-password = 1234
+password = 132
+locked = 1
 
 def on_forever():
-    global input2
+    global input2, locked
     if password == input2:
+        input2 = 0
+        if locked == 1:
+            pins.servo_write_pin(AnalogPin.P8, 0)
+            locked = 0
+        else:
+            pins.servo_write_pin(AnalogPin.P8, 90)
+            locked = 1
         for index2 in range(4):
             basic.show_icon(IconNames.YES)
-            music.play_sound_effect(music.create_sound_effect(WaveShape.SAWTOOTH,
+            music.play_sound_effect(music.create_sound_effect(WaveShape.NOISE,
                     5000,
                     1,
                     255,
@@ -67,5 +98,4 @@ def on_forever():
                                 . . . . .
                                 . . . . .
             """)
-        input2 = 0
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,64 +1,73 @@
-function wrong () {
+function wrong() {
+    
     input2 = 0
     for (let index = 0; index < 3; index++) {
         basic.showIcon(IconNames.No)
         music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 63, 5000, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.UntilDone)
         basic.showLeds(`
             . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        `)
     }
 }
-input.onPinPressed(TouchPin.P0, function () {
+
+input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
+    
     basic.showLeds(`
         . . # . .
-        . # # . .
-        . . # . .
-        . . # . .
-        . # # # .
-        `)
+                . # # . .
+                . . # . .
+                . . # . .
+                . # # # .
+    `)
     if (input2 == 0) {
         input2 = 1
     } else {
         wrong()
     }
+    
 })
-input.onPinPressed(TouchPin.P2, function () {
+input.onPinPressed(TouchPin.P2, function on_pin_pressed_p2() {
+    
     basic.showLeds(`
         . # # # .
-        . . . # .
-        . # # # .
-        . . . # .
-        . # # # .
-        `)
+                . . . # .
+                . # # # .
+                . . . # .
+                . # # # .
+    `)
     if (input2 == 1) {
         input2 = 13
     } else {
         wrong()
     }
+    
 })
-input.onPinPressed(TouchPin.P1, function () {
+input.onPinPressed(TouchPin.P1, function on_pin_pressed_p1() {
+    
     basic.showLeds(`
         . # # # .
-        . . . # .
-        . # # # .
-        . # . . .
-        . # # # .
-        `)
+                . . . # .
+                . # # # .
+                . # . . .
+                . # # # .
+    `)
     if (input2 == 13) {
         input2 = 132
     } else {
         wrong()
     }
+    
 })
 let input2 = 0
 input2 = 0
 let password = 132
 let locked = 1
-basic.forever(function () {
+basic.forever(function on_forever() {
+    
     if (password == input2) {
         input2 = 0
         if (locked == 1) {
@@ -68,16 +77,18 @@ basic.forever(function () {
             pins.servoWritePin(AnalogPin.P8, 90)
             locked = 1
         }
-        for (let index = 0; index < 4; index++) {
+        
+        for (let index2 = 0; index2 < 4; index2++) {
             basic.showIcon(IconNames.Yes)
             music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 5000, 1, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.UntilDone)
             basic.showLeds(`
                 . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                `)
+                                . . . . .
+                                . . . . .
+                                . . . . .
+                                . . . . .
+            `)
         }
     }
+    
 })
